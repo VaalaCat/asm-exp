6a4078b01b799fb57780bbc553f16f7046f05dcd
Cat Vaala <vaalacat@outlook.com>
2021-12-12 00:42:19 +0800

    新增埃氏筛法

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 #include <math.h>
 #include <sys/types.h>
 
-u_int16_t factorSum(u_int16_t x) {
-    x = x/2;
+u_int16_t filter(u_int16_t x) {
+	x = x/2;
 	u_int16_t i, sum;
 	for (i = 1, sum = 0; i < x; i++) {
 		if (x % i == 0) {
@@ -19,13 +19,13 @@ int main() {
 	printf("input\n");
 	scanf("%hu%hu", &min, &max);
 	for (i = 220; i <= max; i++) {
-        if(i == 0) break;
-		u_int16_t facSum = factorSum(i);
-		if (facSum < min || facSum > max) {
+		if(i == 0) break;
+		u_int16_t t = filter(i);
+		if (t < min || t > max) {
 			continue;
 		}
-		if (i == factorSum(facSum) && i < facSum) {
-			printf("%d-%d\n", i, facSum);
+		if (i == filter(t) && i < t) {
+			printf("%d-%d\n", i, t);
 		}
 	}
 	return 0;
