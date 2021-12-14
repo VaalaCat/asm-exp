dd6d6dc7249ea001b52b7e81658c9ea52b1a5ee4
Cat Vaala <vaalacat@outlook.com>
2021-12-14 20:31:47 +0800

    小数成功

--- final.cpp
+++ final.cpp
@@ -1,3 +1,5 @@
+#include<iostream>
+using namespace std;
 unsigned int min_low,max_low;
 unsigned int min_high,max_high;
 
@@ -5,6 +7,7 @@ unsigned int now_high,now_low;
 unsigned int sum_high,sum_low;
 
 unsigned int i_high,i_low;
+unsigned int i1_high,i1_low;
 unsigned int tmp_high,tmp_low;
 
 unsigned int arg_high,arg_low;
@@ -12,34 +15,35 @@ unsigned int arg_high,arg_low;
 unsigned int output_high,output_low;
 
 void getsum(){
-	for (i_high=0;i_high<=arg_high;i_high++){
-		if (i_high == arg_high){
-			for (i_low=0;i_low<=arg_low;i_low++){
+	sum_high=0;sum_low=0;
+	for (i1_high=0;i1_high<=arg_high;i1_high++){
+		if (i1_high == arg_high){
+			for (i1_low=0;i1_low<=arg_low;i1_low++){
 				unsigned int tmp1=(arg_high<<16)+arg_low;
-				unsigned int tmp2=(i_high<<16)+i_low;
-				if (i_high==arg_high && i_low==arg_low){
+				unsigned int tmp2=(i1_high<<16)+i1_low;
+				if (i1_high==arg_high && i1_low==arg_low){
 					return;
 				}
 				if (tmp1 % tmp2==0){ // 伪代码
-					sum_low=sum_low+i_low;
+					sum_low=sum_low+i1_low;
 					unsigned int tmp3=(sum_low>>16);
 					sum_low-=(tmp3<<16);
-					sum_high=sum_high+i_high+tmp3;
+					sum_high=sum_high+i1_high+tmp3;
 				}
 			}
 		}
 		else{
-			for (i_low=0;i_low<=0xffff;i_low++){
+			for (i1_low=0;i1_low<=0xffff;i1_low++){
 				unsigned int tmp1=(arg_high<<16)+arg_low;
-				unsigned int tmp2=(i_high<<16)+i_low;
-				if (i_high==arg_high && i_low==arg_low){
+				unsigned int tmp2=(i1_high<<16)+i1_low;
+				if (i1_high==arg_high && i1_low==arg_low){
 					return;
 				}
 				if (tmp1 % tmp2==0){ // 伪代码
-					sum_low=sum_low+i_low;
+					sum_low=sum_low+i1_low;
 					unsigned int tmp3=(sum_low>>16);
 					sum_low-=(tmp3<<16);
-					sum_high=sum_high+i_high+tmp3;
+					sum_high=sum_high+i1_high+tmp3;
 				}
 			}
 		}
@@ -48,44 +52,85 @@ void getsum(){
 }
 
 int main(){
-	min_low = 0x0001;
+	min_low = 0x00da;
 	min_high = 0x0000;
 	max_low = 0x0000;
-	max_high = 0x0020;
-	arg_high = 0x000f;
-	arg_low = 0x4240;
-	// for (i_high=min_high;i_high<=max_high;i_high++){
-	// 	if (i_high==max_high){
-	// 		for (i_low=min_low;i_low<=max_low;i_low++){
-	// 			arg_high=i_high;
-	// 			arg_low=i_low;
-	// 			getsum();
-	// 			tmp_high=sum_high;
-	// 			tmp_low=sum_low;
-	// 			arg_high=tmp_high;
-	// 			arg_low=tmp_low;
-	// 			getsum();
-	// 			if (tmp_high==sum_high && tmp_low==sum_low){
-	// 				output_high=tmp_high;
-	// 				output_low=tmp_low;
-	// 			}
-	// 		}
-	// 	}else{
-	// 		for (i_low=min_low;i_low<0xffff;i_low++){
-	// 			arg_high=i_high;
-	// 			arg_low=i_low;
-	// 			getsum();
-	// 			tmp_high=sum_high;
-	// 			tmp_low=sum_low;
-	// 			arg_high=tmp_high;
-	// 			arg_low=tmp_low;
-	// 			getsum();
-	// 			if (tmp_high==sum_high && tmp_low==sum_low){
-	// 				output_high=tmp_high;
-	// 				output_low=tmp_low;
-	// 			}
-	// 		}
-	// 	}
-	// }
+	max_high = 0x0002;
+	arg_high = 0x0000;
+	arg_low = 0x0000;
+	for (i_high=min_high;i_high<=max_high;i_high++){
+		if (i_high==min_high && min_high==max_high){
+			for (i_low=min_low;i_low<=max_low;i_low++){
+				arg_high=i_high;
+				arg_low=i_low;
+				getsum();
+				tmp_high=sum_high;
+				tmp_low=sum_low;
+				arg_high=tmp_high;
+				arg_low=tmp_low;
+				getsum();
+				if (i_high==sum_high && i_low==sum_low){
+					output_high=tmp_high;
+					output_low=tmp_low;
+					printf("%x%x-%x%x\n",i_high,i_low,tmp_high,tmp_low);
+				}
+			}
+			break;
+		}
+		if (i_high==min_high && min_high!=max_high){
+			for (i_low=min_low;i_low<=0xffff;i_low++){
+				arg_high=i_high;
+				arg_low=i_low;
+				getsum();
+				tmp_high=sum_high;
+				tmp_low=sum_low;
+				arg_high=tmp_high;
+				arg_low=tmp_low;
+				getsum();
+				if (tmp_high==sum_high && tmp_low==sum_low&& (tmp_high!=0&&tmp_low!=0)){
+					output_high=tmp_high;
+					output_low=tmp_low;
+					printf("%x-%x",output_high,output_low);
+				}
+			}
+			continue;
+		}
+		if (i_high!=min_high && min_high!=max_high && i_high!=max_high){
+			for (i_low=0;i_low<=0xffff;i_low++){
+				arg_high=i_high;
+				arg_low=i_low;
+				getsum();
+				tmp_high=sum_high;
+				tmp_low=sum_low;
+				arg_high=tmp_high;
+				arg_low=tmp_low;
+				getsum();
+				if (tmp_high==sum_high && tmp_low==sum_low&& (tmp_high!=0&&tmp_low!=0)){
+					output_high=tmp_high;
+					output_low=tmp_low;
+					printf("%x-%x",output_high,output_low);
+				}
+			}
+			continue;
+		}
+		if (i_high==max_high){
+			for (i_low=0;i_low<=max_low;i_low++){
+				arg_high=i_high;
+				arg_low=i_low;
+				getsum();
+				tmp_high=sum_high;
+				tmp_low=sum_low;
+				arg_high=tmp_high;
+				arg_low=tmp_low;
+				getsum();
+				if (tmp_high==sum_high && tmp_low==sum_low&& (tmp_high!=0&&tmp_low!=0)){
+					output_high=tmp_high;
+					output_low=tmp_low;
+					printf("%x-%x",output_high,output_low);
+				}
+			}
+			continue;
+		}
+	}
 	return 0;
 }
